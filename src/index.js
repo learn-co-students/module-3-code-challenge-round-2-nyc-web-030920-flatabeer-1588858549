6bbde16c148f81e0ac83be7107513ea204ba2b30
Eliot Henson <eliotrhenson@gmail.com>
2020-05-07 10:09:04 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 // Code here
+
+document.addEventListener('DOMContentLoaded', function (){
+
+    getBeer();
+})
+
+function getBeer() {
+    fetch ("http://localhost:3000/beers/1")
+    .then(resp => resp.json())
+    .then(beer => addBeer(beer))
+}
+
+function addBeer(beer){
+    const beerName = document.getElementsByClassName("beer-details")[0].children[0]
+    beerName.innerText = `${beer.name}`
+    const beerImage = document.getElementsByClassName("beer-details")[0].children[1]
+    beerImage.src = `${beer.image_url}`
+    const beerDescription = document.getElementsByClassName("description")[0].children[0]
+    beerDescription.innerText = `${beer.description}`
+    const beerReview = document.getElementsByClassName("reviews")[0].children[0]
+    const beerReviewTwo = document.getElementsByClassName("reviews")[0].children[1]
+    beerReview.innerText = `${beer.reviews[0]}`
+    beerReviewTwo.innerText = `${beer.reviews[1]}`
+
+}
