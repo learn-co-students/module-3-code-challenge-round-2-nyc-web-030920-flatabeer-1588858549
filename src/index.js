26648a5c566ce15b5dcc4dc8c61ca3d90a7e1088
Anil Gurung <anil_grg@hotmail.com>
2020-05-07 08:11:51 -0600

    working

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 // Code here
+// GET `/beers/[:id]` (start with /beers/1)
+// - PATCH `/beers/[:id]`
+ 
+document.addEventListener('DOMContentLoaded',function(){
+// const beerDetails = document.querySelector(".beer-details")
+    fetch(`http://localhost:3000/beers/1`)
+    .then(resp => resp.json())
+    .then(beer => displayBeer(beer))
+
+    function displayBeer(beer){
+        const beerDetails = document.querySelector(".beer-details")
+
+        const newDiv = document.createElement('div')
+
+
+
+        newDiv.innerHTML += `
+        <h2> ${beer.name} </h2>
+        <img src = "${beer.img_url}">
+        <text area> ${beer.description}</text area>
+        `
+
+        beerDetails.append(newDiv)
+    
+    }
+})
