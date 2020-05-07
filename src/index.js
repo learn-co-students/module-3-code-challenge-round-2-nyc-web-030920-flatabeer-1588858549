ff808d3a2b6460114a6062d575c5bd15163cf318
Anil Gurung <anil_grg@hotmail.com>
2020-05-07 09:19:14 -0600

    done

--- src/index.js
+++ src/index.js
@@ -10,18 +10,62 @@ document.addEventListener('DOMContentLoaded',function(){
 
     function displayBeer(beer){
         const beerDetails = document.querySelector(".beer-details")
+        beerDetails.children[0].innerText = `${beer.name}`
+    //    debugger // beerDetails.innerHTML = '${beer.name}'
+    // debugger
+     beerDetails.children[1].src = `${beer.img_url}`
+     
 
-        const newDiv = document.createElement('div')
+     const formDetail = document.querySelector(".description")
+    //  debugger
+     formDetail.children[0].innerHTML = `${beer.description}`
 
-
-
-        newDiv.innerHTML += `
-        <h2> ${beer.name} </h2>
-        <img src = "${beer.img_url}">
-        <text area> ${beer.description}</text area>
-        `
-
-        beerDetails.append(newDiv)
+   formDetail.addEventListener('click',function(e){
+    // debugger
+    let textArea = e.target.parentElement.children[0].value
     
-    }
+    fetch(`http://localhost:3000/beers/1`,{
+        method: "PATCH",
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            description : textArea
+
+        })
+       })
+
+})
+ 
+
+
+// let reviewForms = document.querySelector(".reviews")
+
+// beer.reviews.forEach(function(beer){
+//     let existingReview = document.createElement('li')
+//     // debugger
+//     existingReview.innerHTML = beer.review
+//     reviewForms.append(existingReview)
+
+// })
+// }
+
+     
+
+
+let reviewForm = document.querySelector(".review-form")
+
+reviewForm.addEventListener('submit',function(e){
+    e.preventDefault();
+    let newText = e.target.children[0].value
+    let ul = document.querySelector('.reviews')
+    let li = document.createElement('li')
+    li.innerText = newText
+   ul.append(li)
+})
+
+    }    
+    
+    
 })
